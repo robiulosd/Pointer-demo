fd0020a23fa2902e3490e26bda66d6eeac6bf0e3
Md. Robiul Hasan <150306663+robiulosd@users.noreply.github.com>
2023-11-16 19:09:46 +0600

    Add files via upload

---
+++ Pointer.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+ {
+  int x = 5;
+  int *ptr;
+  ptr = &x;
+ printf("%d\n",x);
+ printf("%d\n",&x);
+ printf("%d\n",ptr);
+ printf("%d\n",*ptr);
+ printf("%d\n",&ptr);
+
+  return 0;
+}
